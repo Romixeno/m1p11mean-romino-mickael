3f6943375109caa2ad329bda7592a14837c76c1f
R.Mickael <kamirakoto2001@gmail.com>
2024-02-21 13:23:33 +0200

    get revenu per mounth

--- server/ressources/statistiqueRoute.js
+++ server/ressources/statistiqueRoute.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { getAverageWorkTimeForEmployees, getRevenuePerDay } from "../controllers/statistiqueController.js";
+import { getAverageWorkTimeForEmployees, getRevenuePerDay, getRevenuePerMonth } from "../controllers/statistiqueController.js";
 export default Router()
     .get('/averageWorkTime/:employeeId',getAverageWorkTimeForEmployees)
     .get('/getRevenuePerDay/:date',getRevenuePerDay)
+    .get('/revenue-per-month/:year/:month',getRevenuePerMonth)
